58ab01ee073ad2401d08874d4e53b5ffb10cd6ad
dvorobey <dmitriy.vorobey1@icloud.com>
2023-04-11 16:17:03 +0300

    add game initialization

--- src/Fallout.Shelter.Core/Models/Building.cs
+++ src/Fallout.Shelter.Core/Models/Building.cs
@@ -2,13 +2,23 @@ namespace Fallout.Shelter.Core.Models;
 
 public class Building
 {
+    public string Name { get; set; }
     public Dictionary<CurrencyType, int> Price { get; set; }
     public bool IsElevator { get; set; }
     public Space[] Spaces { get; set; }
-
-    public Building(Dictionary<CurrencyType, int> price)
+    
+    public Building(string name, bool isElevator = false)
+    {
+        Name = name;
+        IsElevator = isElevator;
+        Price = new Dictionary<CurrencyType, int>();
+        Spaces = new Space[isElevator ? 1 : 2];
+    }
+    
+    public Building(string name, Dictionary<CurrencyType, int> price)
     {
         Price = price;
+        Name = name;
         Spaces = new Space[2];
     }
 }
--- src/Fallout.Shelter.Core/Models/GameField.cs
+++ src/Fallout.Shelter.Core/Models/GameField.cs
@@ -11,6 +11,18 @@ public class GameField
             throw new ArgumentOutOfRangeException(nameof(playersCount), "Maximum players count is 4 and minimum is 2");
         }
 
-        Field = new Building[playersCount + 1, 13];
+        Field = new Building[playersCount + 1, 7];
+        Field[0, 0] = new Building("The Wasteland");
+        Field[0, 1] = new Building("Vault Door");
+        Field[0, 2] = new Building("Vault Entrance");
+        Field[0, 3] = new Building("Elevator",true);
+        Field[0, 4] = new Building("Water Treatment");
+        Field[0, 5] = new Building("Diner");
+        Field[0, 6] = new Building("Power Generator");
+
+        for (var i = 1; i < playersCount; i++)
+        {
+            Field[i, 7] = new Building("Elevator",true);
+        }
     }
 }

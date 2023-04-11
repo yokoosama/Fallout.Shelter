b49bfb9f5cab2a6c1e99f62829d0db4af747291c
dvorobey <dmitriy.vorobey1@icloud.com>
2023-04-11 17:01:50 +0300

    fix condition

--- src/Fallout.Shelter.Core/Models/Condition.cs
+++ src/Fallout.Shelter.Core/Models/Condition.cs
@@ -6,7 +6,7 @@ public class Condition
     public int Happiness { get; set; }
     public bool IsItemNeeded { get; set; }
     public bool ShouldDwellerBeInjured { get; set; }
-    public bool InjureDweller { get; set; }
+    public bool ShouldDwellerBeHealed { get; set; }
     public bool ShouldBuild { get; set; }
     public int Roll { get; set; }
 
